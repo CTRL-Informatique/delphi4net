14136620b320159ab395de9cca2dd95e717a9b79
RAPMAR <rmarcouxtremblay@ctrl.com>
2022-07-18 09:50:55 -0400

    added tests for the resolver

--- Net/d4net.Tests/DelphiDllResolverTests.cs
+++ Net/d4net.Tests/DelphiDllResolverTests.cs
@@ -13,18 +13,34 @@ public class DelphiDllResolverTests
     [TestInitialize]
     public void _Initialize() {
         _serviceCollection = new ServiceCollection();
+    }
 
+    [TestMethod]
+    public void ShouldTheDllAssociatedWithTheName() {
         _serviceCollection.AddDelphi4Net()
             .AddDelphiDll<FakeDelphiDll_1>("1")
             .AddDelphiDll<FakeDelphiDll_2>("2");
 
         _serviceProvider = _serviceCollection.BuildServiceProvider();
         _resolver = _serviceProvider.GetRequiredService<IDelphiDllResolver>();
+
+        _resolver.Resolve("1").Should().BeOfType<FakeDelphiDll_1>();
+        _resolver.Resolve("2").Should().BeOfType<FakeDelphiDll_2>();
     }
 
     [TestMethod]
-    public void ShouldTheDllAssociatedWithTheName() {
-        _resolver.Resolve("1").Should().BeOfType<FakeDelphiDll_1>();
-        _resolver.Resolve("2").Should().BeOfType<FakeDelphiDll_2>();
+    public void ShouldWorkWithoutDllName() {
+        _serviceCollection.AddDelphi4Net().AddDelphiDll<FakeDelphiDll_1>();
+        _serviceProvider = _serviceCollection.BuildServiceProvider();
+        _resolver = _serviceProvider.GetRequiredService<IDelphiDllResolver>();
+        _resolver.Resolve("").Should().BeOfType<FakeDelphiDll_1>();
+    }
+
+    [TestMethod]
+    [ExpectedException(typeof(ArgumentException))]
+    public void ShouldThrowIfTwoDllsRegisteredWithoutAName() {
+        _serviceCollection.AddDelphi4Net()
+            .AddDelphiDll<FakeDelphiDll_1>()
+            .AddDelphiDll<FakeDelphiDll_2>();
     }
 }
